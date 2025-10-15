90fef26a29beeb639dff746cc22690eb98aaca72
Zeeshan Khan (CI Bot) <zkhan@360healthtek.com>
2025-10-15 16:53:58 +0500

    refactor: Refactors

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,6 @@ function App() {
       <p className="read-the-docs">Chores</p>
       <p className="read-the-docs">Documentation</p>
       <p className="read-the-docs">Refactors</p>
-      <p className="read-the-docs">Styles</p>
-      <p className="read-the-docs">Tests</p>
     </>
   );
 }
