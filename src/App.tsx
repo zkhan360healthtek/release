ccd88acb91f2049c59cd0586a36263007a89f3f2
Zeeshan Khan (CI Bot) <zkhan@360healthtek.com>
2025-10-15 16:00:37 +0500

    feat: Features

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">Features</p>
     </>
   );
 }
