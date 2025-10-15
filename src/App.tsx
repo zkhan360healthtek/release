e3e19e924fdd0a4cc99004e0f1557d930a7c79a8
zkhan360healthtek <zkhan@360healthtek.com>
2025-10-15 15:10:45 +0500

    feat: all new page 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
